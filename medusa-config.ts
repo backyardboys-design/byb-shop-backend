6b998eb4641f8116c3b24b76f5aafc843094987d
Matthias Streicher <matthias-streicher@hotmail.com>
2026-04-01 22:44:41 +0200

    Update medusa-config.ts

--- medusa-config.ts
+++ medusa-config.ts
@@ -1,63 +1,25 @@
-import path from "path";
-import { loadEnv, defineConfig, Modules } from "@medusajs/framework/utils";
+import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 
-loadEnv(process.env.NODE_ENV || "development", process.cwd());
+loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     redisUrl: process.env.REDIS_URL,
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
     databaseDriverOptions: {
       ssl: false,
       sslmode: "disable",
     },
-    workerMode: process.env.MEDUSA_WORKER_MODE as
-      | "shared"
-      | "worker"
-      | "server",
     http: {
-      storeCors: process.env.STORE_CORS,
-      adminCors: process.env.ADMIN_CORS,
-      authCors: process.env.AUTH_CORS,
-      jwtSecret: process.env.JWT_SECRET,
-      cookieSecret: process.env.COOKIE_SECRET,
-    },
+      storeCors: process.env.STORE_CORS!,
+      adminCors: process.env.ADMIN_CORS!,
+      authCors: process.env.AUTH_CORS!,
+      jwtSecret: process.env.JWT_SECRET || "supersecret",
+      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
+    }
   },
-  modules: [
-    {
-      resolve: "@medusajs/medusa/file",
-      options: {
-        providers: [
-          {
-            resolve: "@medusajs/medusa/file-local",
-            id: "local",
-            options: {
-              upload_dir: "static",
-              backend_url: "https://api.backyardboys.at/static",
-            },
-          },
-        ],
-      },
-    },
-  ],
-
   admin: {
-    backendUrl: "https://api.backyardboys.at",
-    vite: () => ({
-      resolve: {
-        alias: {
-          "/src": path.resolve(process.cwd(), "src"),
-        },
-      },
-      server: {
-        host: "0.0.0.0",
-        allowedHosts: [
-          "api.backyardboys.at",
-          "localhost",
-          ".localhost",
-          "127.0.0.1",
-        ],
-      },
-    }),
+    disable: process.env.DISABLE_MEDUSA_ADMIN === "true",
   },
-});
+})
